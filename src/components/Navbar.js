55321ff588d00716e387a66979491c104168743f
frozzi18 <42233054+frozzi18@users.noreply.github.com>
2021-04-10 03:26:46 +0700

    Finish Project

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ class Navbar extends Component {
 
   render() {
     return (
-      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+      <nav className="navbar navbar-dark fixed-top bg-info flex-md-nowrap p-0 shadow">
         <a
           className="navbar-brand col-sm-3 col-md-2 mr-0"
           href="http://www.dappuniversity.com/bootcamp"
@@ -13,7 +13,7 @@ class Navbar extends Component {
           rel="noopener noreferrer"
         >
           <img src={farmer} width="30" height="30" className="d-inline-block align-top" alt="" />
-          &nbsp; DApp Token Farm
+          &nbsp; Froz DApp Token Farm
         </a>
 
         <ul className="navbar-nav px-3">
